8027a07c0685bdcbfe0c97768870f51960578df0
FrisoDev <frisodevstoker@gmail.com>
2017-10-10 12:09:28 +0200

    create events and user and relation

---
+++ app/models/event.rb
@@ -0,0 +1,12 @@
+class Event < ApplicationRecord
+  validates :address, presence: true
+  validates :home_type, presence: true
+  validates :room_type, presence: true
+  validates :accommodate, presence: true
+  validates :bedroom_count, presence: true
+  validates :bathroom_count, presence: true
+  validates :listing_name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 500 }
+  
+ belongs_to :user
+end
