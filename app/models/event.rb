ef57dde1072dc2b88e7ae2d3014e09da55aae0e2
FrisoDev <frisodevstoker@gmail.com>
2017-10-10 12:11:24 +0200

    correct validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,8 @@
 class Event < ApplicationRecord
-  validates :address, presence: true
-  validates :home_type, presence: true
-  validates :room_type, presence: true
-  validates :accommodate, presence: true
-  validates :bedroom_count, presence: true
-  validates :bathroom_count, presence: true
-  validates :listing_name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 500 }
-  
+  price (0)
+includes_food (false)
+includes_drinks (false)
+active (true)
+
  belongs_to :user
 end
