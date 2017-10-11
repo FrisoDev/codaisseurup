c77d2515a51e90c6d19078194eca48643c2936dc
FrisoDev <frisodevstoker@gmail.com>
2017-10-11 14:39:05 +0200

    nav

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,18 +6,18 @@ FactoryGirl.define do
     description "Come play lacrosse for the first time in Amsterdam.
     Let´s play a game,men and women, boys and girls."
     location "Amsterdam"
-    price { Faker::Commerce.price }
+    price  Faker::Commerce.price
     includes_food true
     includes_drinks true
     starts_at DateTime.now
     ends_at DateTime.now
     active true
-    categories [swimming]
+    # categories [games]
     user { build(:user) }
 
 
     trait :active do
-      active truen
+      active true
     end
 
     trait :inactive do
